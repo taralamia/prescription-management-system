f2efa2151c59ae66d478914d52745c58b5fe6a3e
unknown <taralamia69@gmail.com>
2025-11-05 03:07:11 +0600

    Initial commit of my project

---
+++ src/main/java/com/example/prescriptionapp/PrescriptionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.prescriptionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrescriptionAppApplication.class, args);
+    }
+
+}
